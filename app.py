1c23e12e48b22b67509abda41bbe1d56ad213433
EC2 Default User <ec2-user@ip-172-31-23-151.eu-west-1.compute.internal>
2022-06-21 11:11:11 +0000

    Test of automatic deployment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "AWS Masters!"
+    return "AWS Masters! Firs update test"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))
